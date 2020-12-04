14da728de044dfa61e3077f675cdbe3ac88139ea
Xeltor <Xeltor@users.noreply.github.com>
2020-12-04 23:05:02 +0100

    Update hs265ize.sh

--- scripts/hs265ize.sh
+++ scripts/hs265ize.sh
@@ -5,4 +5,4 @@ HOSTNAME="$(hostname)"
 && curl --header "Content-Type: application/json" \
 --request POST \
 --data '{"containerId": "'${HOSTNAME}'"}' \
-http://172.17.0.1:5678/webhook-test/transcoder_status
+http://172.17.0.1:5678/webhook/transcoder_status
